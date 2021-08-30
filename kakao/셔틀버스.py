f219e5d17025735946dfd9895f9648b114d4e125
gonipark <gonipark25@gmail.com>
2021-08-31 02:01:23 +0900

    0831

--- kakao/셔틀버스.py
+++ kakao/셔틀버스.py
@@ -1,8 +1,5 @@
-#50점
-
 import datetime as dt
 def solution(n, t, m, timetable):
-    answer = ''
     timetable.sort()
 
     now=dt.datetime.strptime('09:00', "%H:%M")
@@ -14,29 +11,34 @@ def solution(n, t, m, timetable):
     full=0
     turn=0
 
+    people_left=False
     for j in range(len(timetable)):
-        if full==m:
-            turn+=1
-            full=0
-        if turn==len(bus):
-            full=0
+        if full == m:
+            turn += 1
+            if turn==len(bus):
+                break
+            full = 0
+
+        if turn == len(bus):
+            if dt.datetime.strptime(timetable[j], "%H:%M") <= bus[turn - 1]:
+                people_left = True
             break
 
-        if dt.datetime.strptime(timetable[j], "%H:%M")<=bus[turn] and full<m:
+        if dt.datetime.strptime(timetable[j], "%H:%M")<=bus[turn]:
             full+=1
             last=dt.datetime.strptime(timetable[j], "%H:%M")
-        elif dt.datetime.strptime(timetable[j], "%H:%M")>bus[turn]:
+        else:
             turn+=1
             full=0
 
-            for i in bus[turn:]:
+            for index,i in enumerate(bus[turn:]):
                 if dt.datetime.strptime(timetable[j], "%H:%M")<=i:
                     full=1
+                    turn+=index
                     break
             last=dt.datetime.strptime(timetable[j], "%H:%M")
-            #print(timetable[j])
 
-    if full<m:
+    if full<m and (not people_left):
         answer=str(bus[-1])[11:16]
     else:
         answer=str(last-dt.timedelta(minutes=1))[11:16]
@@ -45,9 +47,12 @@ def solution(n, t, m, timetable):
 
     return answer
 
-print(solution(1,	1	,5	,["08:00", "08:01", "08:02", "08:03"]))
-print(solution(2,	10,	2,	["09:10", "09:09", "08:00"]))
-print(solution(2,	1	,2	,["09:00", "09:00", "09:00", "09:00"]))
-print(solution(1,	1	,5,	["00:01", "00:01", "00:01", "00:01", "00:01"]))
-print(solution(1,	1	,1,	["23:59"]))
-print(solution(10,	60	,45,	["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"]))
+# print(solution(2,	10	,2	,["08:00", "08:01", "08:02", "08:03","09:01"]))
+# print(solution(2,	10,	2,	["09:10", "09:09", "08:00"]))
+print(solution(1, 1, 1, ["09:00", "09:05"])) #8:59
+#print(solution(2, 1, 1, ["09:00", "09:03"]))
+# print(solution(2,	1	,2	,["09:00", "09:00", "09:00", "09:00"]))
+# print(solution(1,	1	,5,	["00:01", "00:01", "00:01", "00:01", "00:01"]))
+# print(solution(1,	1	,1,	["23:59"]))
+# print(solution(10,	60	,45,	["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"]))
+
